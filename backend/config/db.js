cc269356e4141ee132661f0c220c24087a8fe922
AMUC Adhikari <21it0451@itum.mrt.ac.lk>
2025-09-22 13:17:30 +0530

    Create Routes,controllers and requests

--- backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import {neon} from "@neondatabase/serverless";
-
-import "dotenv/config.js";
-
-export const sql = neon(process.env.DATABASE_URL)
---
+++ backend/src/server.js
@@ -0,0 +1,27 @@
+// const express = require('express')
+import express from 'express';
+import dotenv from "dotenv";
+import { initDB } from "./config/db.js"
+import rateLimiter from './middleware/rateLimiter.js';
+import transactionsRoute from "./routes/transactionsRoute.js";
+
+dotenv.config();
+
+const app = express();
+
+//middleware
+app.use(rateLimiter);
+app.use(express.json());
+
+const PORT = process.env.PORT || 5001;
+
+
+app.use("/api/transactions", transactionsRoute);
+
+
+
+initDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server is running on PORT:", PORT);
+    });
+});
